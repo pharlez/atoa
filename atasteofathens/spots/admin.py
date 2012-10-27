316bb77141a91f32ff563329db98c84c103ea477
greenonion <sublime.gr@gmail.com>
2012-10-27 13:52:11 +0300

    Renamed ratings to spots

---
+++ atasteofathens/spots/admin.py
@@ -0,0 +1,7 @@
+from atasteofathens.spots.models import Neighbourhood, RestaurantCat, Restaurant, Rating
+from django.contrib import admin
+
+admin.site.register(Neighbourhood)
+admin.site.register(RestaurantCat)
+admin.site.register(Restaurant)
+admin.site.register(Rating)
