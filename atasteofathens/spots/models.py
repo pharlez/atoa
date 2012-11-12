23849742a1a1a507a8b1044e0e42f49385f64d6a
greenonion <sublime.gr@gmail.com>
2012-11-12 20:57:53 +0200

    changing models

--- atasteofathens/spots/models.py
+++ atasteofathens/spots/models.py
@@ -14,11 +14,12 @@ class Neighbourhood(models.Model):
 class Place(models.Model):
     address = models.CharField(max_length=200)
     neighbourhood = models.ForeignKey(Neighbourhood)
+    phone = models.CharField(max_length=20, blank=True)
 
     class Meta:
         abstract = True
 
-class RestaurantCat(models.Model):
+class EatCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     class Meta:
@@ -27,9 +28,8 @@ class RestaurantCat(models.Model):
     def __unicode__(self):
         return self.name
 
-class Restaurant(Place):
+class Eat(Place):
     name = models.CharField(max_length=200)
-    phone = models.CharField(max_length=20, blank=True)
     category = models.ForeignKey(RestaurantCat)
     PRICE_RANGES = (
             (1, '$'),
@@ -40,21 +40,93 @@ class Restaurant(Place):
     )
     price = models.PositiveSmallIntegerField(choices=PRICE_RANGES, null=True, blank=True)
     # hours
-    # attire
-    good_for_groups = models.BooleanField(blank=True)
-    takes_reservations =  models.BooleanField(blank=True)
+    ATTIRE_RANGES = (
+            (1, 'casual'),
+            (2, 'smart'),
+            (3, 'formal'),
+    )
+    attire = models.PositiveSmallIntegerField(choices=ATTIRE_RANGES, null=True, blank=True)
+    group_friendly = models.BooleanField(blank=True)
+    date_friendly = models.BooleanField(blank=True)
+    reservations =  models.BooleanField(blank=True)
     delivery = models.BooleanField(blank=True)
+    waiter_service = models.BooleanField(blank=True)
     take_out = models.BooleanField(blank=True)
     wi_fi = models.BooleanField(blank=True)
-    # goodfor
+    credit_card = models.BooleanField(blank=True)
+    tv = models.BooleanField(blank=True)
+    smoking = models.BooleanField(blank=True)
+    # alcohol
+    # music
     NOISE_RANGES = (
             (1, 'low'),
             (2, 'average'),
             (3, 'high'),
     )
     noise_level = models.PositiveSmallIntegerField(choices=NOISE_RANGES, blank=True, null=True)
-    # ambience
     wheelchair = models.BooleanField(blank=True)
+    website = models.CharField(max_length=100)
+    # create slug for urls
+    slug = models.SlugField(blank=True, db_index=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def save(self, **kwargs):
+        slug_str = "%s" % (self.name)
+        unique_slugify(self, slug_str)
+        super(Restaurant, self).save()
+
+    def __unicode__(self):
+        return self.name
+
+class DrinkCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
+
+class Drink(Place):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(DrinkCategory)
+    PRICE_RANGES = (
+            (1, '$'),
+            (2, '$$'),
+            (3, '$$$'),
+            (4, '$$$$'),
+            (5, '$$$$$'),
+    )
+    price = models.PositiveSmallIntegerField(choices=PRICE_RANGES, null=True, blank=True)
+    # hours
+    ATTIRE_RANGES = (
+            (1, 'casual'),
+            (2, 'smart'),
+            (3, 'formal'),
+    )
+    attire = models.PositiveSmallIntegerField(choices=ATTIRE_RANGES, null=True, blank=True)
+    group_friendly = models.BooleanField(blank=True)
+    date_friendly = models.BooleanField(blank=True)
+    reservations =  models.BooleanField(blank=True)
+    delivery = models.BooleanField(blank=True)
+    waiter_service = models.BooleanField(blank=True)
+    take_out = models.BooleanField(blank=True)
+    wi_fi = models.BooleanField(blank=True)
+    credit_card = models.BooleanField(blank=True)
+    tv = models.BooleanField(blank=True)
+    smoking = models.BooleanField(blank=True)
+    # alcohol
+    # music
+    NOISE_RANGES = (
+            (1, 'low'),
+            (2, 'average'),
+            (3, 'high'),
+    )
+    noise_level = models.PositiveSmallIntegerField(choices=NOISE_RANGES, blank=True, null=True)
+    wheelchair = models.BooleanField(blank=True)
+    website = models.CharField(max_length=100)
     # create slug for urls
     slug = models.SlugField(blank=True, db_index=True)
 
@@ -72,7 +144,13 @@ class Restaurant(Place):
 class Rating(models.Model):
     restaurant = models.ForeignKey(Restaurant, related_name='r_ratings')
     user = models.ForeignKey(AuthUser, related_name='u_ratings')
-    rating_value = models.PositiveSmallIntegerField('Rating')
+    RATING_RANGES = (
+            (0, 'bad'),
+            (1, 'OK'),
+            (2, 'good'),
+    )
+    rating_value = models.PositiveSmallIntegerField('Rating', choices=RATING_RANGES)
+    #rating_time = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return self.user.username + " has rated '" + self.restaurant.name + "'"
--- atasteofathens/spots/views.py
+++ atasteofathens/spots/views.py
@@ -23,18 +23,19 @@ def user_profile(request, user_name):
 def user_rate(request, user_name):
     u = get_object_or_404(User, username=user_name)
 
-    all_items = Restaurant.objects.all()
+    #all_items = Restaurant.objects.all()
 
     user_ratings = [(i.restaurant, i.rating_value) for i in u.u_ratings.all()]
-    rated_items = [x[0] for x in user_ratings]
-    no_rating = [(i, None) for i in all_items if i not in rated_items]
+    #rated_items = [x[0] for x in user_ratings]
+    #no_rating = [(i, None) for i in all_items if i not in rated_items]
 
-    user_ratings = user_ratings + no_rating
-    user_ratings.sort(key=lambda item: item[0].name)
+    #user_ratings = user_ratings + no_rating
+    #user_ratings.sort(key=lambda item: item[0].name)
 
     return render(request, 'spots/user_rate.html', 
                              {'user': u,
                               'user_ratings': user_ratings})
+
 def restaurants(request):
     restaurant_list = Restaurant.objects.all()
     return render(request, 'spots/restaurant_list.html', {'item_list': restaurant_list})
