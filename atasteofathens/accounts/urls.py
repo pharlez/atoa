a83a58557ac393adf8df5cfcb37384820c00db15
greenonion <sublime.gr@gmail.com>
2012-12-01 22:12:51 +0200

    logout redirects to index page

--- atasteofathens/accounts/urls.py
+++ atasteofathens/accounts/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
         url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
-        url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'accounts/logout.html'}),
+        url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'spots/index.html'}),
 )
