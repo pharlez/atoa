a1e68c24dafad96a5e07a6f761e993ab7c4c6701
greenonion <sublime.gr@gmail.com>
2012-12-01 14:10:43 +0200

    made settings independent of system

--- atasteofathens/settings.py
+++ atasteofathens/settings.py
@@ -61,7 +61,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "/home/greenonion/dvp/django/atasteofathens/media",
+    "./media",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -101,7 +101,7 @@ ROOT_URLCONF = 'atasteofathens.urls'
 WSGI_APPLICATION = 'atasteofathens.wsgi.application'
 
 TEMPLATE_DIRS = (
-        '/home/greenonion/dvp/django/atasteofathens/templates',
+        './templates',
 )
 
 INSTALLED_APPS = (
