8c38666759b9db72f781a2ca27365509ad1597ec
greenonion <sublime.gr@gmail.com>
2012-12-01 18:30:27 +0200

    added eat fields

--- atasteofathens/spots/models.py
+++ atasteofathens/spots/models.py
@@ -10,21 +10,11 @@ class Spot(Document):
     address = StringField(max_length=200)
     neighbourhood = StringField(max_length=200)
     phone = StringField(max_length=20)
+    website = StringField(max_length=200)
     slug = AutoSlugField(populate_from='name')
 
-"""
-class EatCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    class Meta:
-        ordering = ['name']
-
-    def __unicode__(self):
-        return self.name
-
-class Restaurant(Place):
-    name = models.CharField(max_length=200)
-    category = models.ForeignKey(RestaurantCat)
+class Eat(EmbeddedDocument):
+    category = StringField(max_length=100)
     PRICE_RANGES = (
             (1, '$'),
             (2, '$$'),
@@ -32,48 +22,34 @@ class Restaurant(Place):
             (4, '$$$$'),
             (5, '$$$$$'),
     )
-    price = models.PositiveSmallIntegerField(choices=PRICE_RANGES, null=True, blank=True)
-    # hours
+    price = IntField(choices=PRICE_RANGES)
     ATTIRE_RANGES = (
             (1, 'casual'),
             (2, 'smart'),
             (3, 'formal'),
     )
-    attire = models.PositiveSmallIntegerField(choices=ATTIRE_RANGES, null=True, blank=True)
-    group_friendly = models.BooleanField(blank=True)
-    date_friendly = models.BooleanField(blank=True)
-    reservations =  models.BooleanField(blank=True)
-    delivery = models.BooleanField(blank=True)
-    waiter_service = models.BooleanField(blank=True)
-    take_out = models.BooleanField(blank=True)
-    wi_fi = models.BooleanField(blank=True)
-    credit_card = models.BooleanField(blank=True)
-    tv = models.BooleanField(blank=True)
-    smoking = models.BooleanField(blank=True)
-    # alcohol
-    # music
+    attire = IntField(choices=ATTIRE_RANGES)
+    group_friendly = BooleanField()
+    date_friendly = BooleanField()
+    reservations = BooleanField()
+    delivery = BooleanField()
+    waiter_service = BooleanField()
+    take_out = BooleanField()
+    wi_fi = BooleanField()
+    credit_card = BooleanField()
+    tv = BooleanField()
+    smoking = BooleanField()
     NOISE_RANGES = (
             (1, 'low'),
             (2, 'average'),
             (3, 'high'),
     )
-    noise_level = models.PositiveSmallIntegerField(choices=NOISE_RANGES, blank=True, null=True)
-    wheelchair = models.BooleanField(blank=True)
-    website = models.CharField(max_length=100)
-    # create slug for urls
-    slug = models.SlugField(blank=True, db_index=True)
+    noise_level = IntField(choices=NOISE_RANGES)
+    wheelchair = BooleanField()
+    # alcohol?
+    # music?
 
-    class Meta:
-        ordering = ['name']
-
-    def save(self, **kwargs):
-        slug_str = "%s" % (self.name)
-        unique_slugify(self, slug_str)
-        super(Restaurant, self).save()
-
-    def __unicode__(self):
-        return self.name
-
+"""
 class DrinkCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
