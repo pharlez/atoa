3401d1306f61ce87a4dcb53bdbc816cac01daeb4
greenonion <sublime.gr@gmail.com>
2012-12-01 20:03:02 +0200

    updating models

--- atasteofathens/spots/models.py
+++ atasteofathens/spots/models.py
@@ -11,10 +11,7 @@ class Spot(Document):
     neighbourhood = StringField(max_length=200)
     phone = StringField(max_length=20)
     website = StringField(max_length=200)
-    slug = AutoSlugField(populate_from='name')
-
-class Eat(EmbeddedDocument):
-    category = StringField(max_length=100)
+    """
     PRICE_RANGES = (
             (1, '$'),
             (2, '$$'),
@@ -23,20 +20,9 @@ class Eat(EmbeddedDocument):
             (5, '$$$$$'),
     )
     price = IntField(choices=PRICE_RANGES)
-    ATTIRE_RANGES = (
-            (1, 'casual'),
-            (2, 'smart'),
-            (3, 'formal'),
-    )
-    attire = IntField(choices=ATTIRE_RANGES)
-    group_friendly = BooleanField()
-    date_friendly = BooleanField()
-    reservations = BooleanField()
-    delivery = BooleanField()
-    waiter_service = BooleanField()
-    take_out = BooleanField()
     wi_fi = BooleanField()
     credit_card = BooleanField()
+    wheelchair = BooleanField()
     tv = BooleanField()
     smoking = BooleanField()
     NOISE_RANGES = (
@@ -45,72 +31,37 @@ class Eat(EmbeddedDocument):
             (3, 'high'),
     )
     noise_level = IntField(choices=NOISE_RANGES)
-    wheelchair = BooleanField()
-    # alcohol?
-    # music?
-
-"""
-class DrinkCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    class Meta:
-        ordering = ['name']
-
-    def __unicode__(self):
-        return self.name
-
-class Drink(Place):
-    name = models.CharField(max_length=200)
-    category = models.ForeignKey(DrinkCategory)
-    PRICE_RANGES = (
-            (1, '$'),
-            (2, '$$'),
-            (3, '$$$'),
-            (4, '$$$$'),
-            (5, '$$$$$'),
-    )
-    price = models.PositiveSmallIntegerField(choices=PRICE_RANGES, null=True, blank=True)
-    # hours
     ATTIRE_RANGES = (
             (1, 'casual'),
             (2, 'smart'),
             (3, 'formal'),
     )
-    attire = models.PositiveSmallIntegerField(choices=ATTIRE_RANGES, null=True, blank=True)
-    group_friendly = models.BooleanField(blank=True)
-    date_friendly = models.BooleanField(blank=True)
-    reservations =  models.BooleanField(blank=True)
-    delivery = models.BooleanField(blank=True)
-    waiter_service = models.BooleanField(blank=True)
-    take_out = models.BooleanField(blank=True)
-    wi_fi = models.BooleanField(blank=True)
-    credit_card = models.BooleanField(blank=True)
-    tv = models.BooleanField(blank=True)
-    smoking = models.BooleanField(blank=True)
-    # alcohol
-    # music
-    NOISE_RANGES = (
-            (1, 'low'),
-            (2, 'average'),
-            (3, 'high'),
-    )
-    noise_level = models.PositiveSmallIntegerField(choices=NOISE_RANGES, blank=True, null=True)
-    wheelchair = models.BooleanField(blank=True)
-    website = models.CharField(max_length=100)
-    # create slug for urls
-    slug = models.SlugField(blank=True, db_index=True)
+    attire = IntField(choices=ATTIRE_RANGES)
+    waiter_service = BooleanField()
+    live_music = BooleanField()
+    group_friendly = BooleanField()
+    date_friendly = BooleanField()
+    reservations = BooleanField()
+    slug = AutoSlugField(populate_from='name')
+    """
+    # music?
+    # location?
 
-    class Meta:
-        ordering = ['name']
+#class Details(EmbeddedDocument):
+    # food details
+   # category = StringField(max_length=100)
+   # delivery = BooleanField()
+    #take_out = BooleanField()
+    ## alcohol?
+    ## hours?
 
-    def save(self, **kwargs):
-        slug_str = "%s" % (self.name)
-        unique_slugify(self, slug_str)
-        super(Restaurant, self).save()
+    # bar details
 
-    def __unicode__(self):
-        return self.name
+    # coffee details
 
+    # club details
+
+"""
 class Rating(models.Model):
     restaurant = models.ForeignKey(Restaurant, related_name='r_ratings')
     user = models.ForeignKey(AuthUser, related_name='u_ratings')
